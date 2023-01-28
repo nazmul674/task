599ad4d4b07a6e42e75af718c1760e9029a1591c
finless88 <yanankhan46@gmail.com>
2023-01-29 02:32:10 +0600

    bug fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("Model press testing..okay");
 var modal = document.getElementById("myModal");
 var innerbtn = document.getElementById("innerbtn");
 var button = document.getElementById("myBtn");
-
+var upldstatus = document.getElementById("upldstatus");
 var span = document.getElementsByClassName("close")[0];
 
 button.onclick = function () {
@@ -11,11 +11,23 @@ button.onclick = function () {
 
 span.onclick = function () {
   modal.style.display = "none";
+  document.getElementById("upldstatus").style.color = "green";
+  upldstatus.textContent = " Upload Status";
 };
+
+let btn = document.getElementById("btnid");
+btn.addEventListener("click", function handleClick() {
+  document.getElementById("upldstatus").style.color = "red";
+  upldstatus.textContent = " File is Uploaded.Please Refresh";
+
+  // alert("Please Refresh the page!");
+});
 
 // modal window close even if anywhere outside model clicked
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    document.getElementById("upldstatus").style.color = "green";
+    upldstatus.textContent = " Upload Status";
   }
 };
