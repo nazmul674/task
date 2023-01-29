e5a5c833267ea9c57f57bc1a733a150ac5e193f4
finless88 <yanankhan46@gmail.com>
2023-01-29 17:30:04 +0600

    bug fixed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// TASK 2
+// JS MODAL just for the first attachment
+
 console.log("Model press testing..okay");
 var modal = document.getElementById("myModal");
-var innerbtn = document.getElementById("innerbtn");
+
 var button = document.getElementById("myBtn");
 var upldstatus = document.getElementById("upldstatus");
 var span = document.getElementsByClassName("close")[0];
@@ -11,14 +14,14 @@ button.onclick = function () {
 
 span.onclick = function () {
   modal.style.display = "none";
-  document.getElementById("upldstatus").style.color = "green";
-  upldstatus.textContent = " Upload Status";
+  // document.getElementById("upldstatus").style.color = "green";
+  // upldstatus.textContent = " Upload Status";
 };
 
 let btn = document.getElementById("btnid");
 btn.addEventListener("click", function handleClick() {
-  document.getElementById("upldstatus").style.color = "red";
-  upldstatus.textContent = " File is Uploaded.Please Refresh";
+  document.getElementById("upldstatus").style.color = "green";
+  upldstatus.textContent = " File is Uploaded.Please Refresh the page!";
 
   // alert("Please Refresh the page!");
 });
@@ -27,7 +30,55 @@ btn.addEventListener("click", function handleClick() {
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    document.getElementById("upldstatus").style.color = "green";
-    upldstatus.textContent = " Upload Status";
   }
 };
+
+function preview() {
+  var totalFiles1 = $("#fileImg1").get(0).files.length;
+
+  for (var i = 0; i < totalFiles1; i++) {
+    $("#preview").append(
+      "<img src = '" + URL.createObjectURL(event.target.files[i]) + "'>"
+    );
+  }
+}
+
+// TASK 3
+/* File Uploading API just for the first attachment*/
+function submitData() {
+  $(document).ready(function () {
+    var formData = new FormData();
+    var filesLength1 = document.getElementById("fileImg1").files.length;
+    for (var i = 0; i < filesLength1; i++) {
+      formData.append(
+        "fileImg[]",
+        document.getElementById("fileImg1").files[i]
+      );
+    }
+    var filesLength2 = document.getElementById("fileImg2").files.length;
+    for (var i = 0; i < filesLength2; i++) {
+      formData.append(
+        "fileImg[]",
+        document.getElementById("fileImg2").files[i]
+      );
+    }
+    var filesLength3 = document.getElementById("fileImg3").files.length;
+    for (var i = 0; i < filesLength3; i++) {
+      formData.append(
+        "fileImg[]",
+        document.getElementById("fileImg3").files[i]
+      );
+    }
+    $.ajax({
+      url: "http://localhost/PHP/Task/filesubmit.php",
+      type: "post",
+      data: formData,
+      contentType: false,
+      processData: false,
+      success: function (response) {
+        alert(response);
+        // window.alert("Win");
+      },
+    });
+  });
+}
